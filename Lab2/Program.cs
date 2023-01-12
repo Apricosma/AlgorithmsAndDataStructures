31869b148baad4f2edf92922c5460ac4565f22eb
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-12 15:12:22 -0600

    Create project

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void RepeatCharacters(string input)
+    {
+
+    }
+
+    static void Main(string[] args)
+    {
+        string repeatCharactersString = "Programmatic Python";
+
+        Console.WriteLine(RepeatCharacters(repeatCharactersString);
+    }
+}
