5f460eed45b612abdf47ef24a193c0e4cfdc9c3c
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-12 16:02:06 -0600

    Add printDuplicates() method

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,14 +1,32 @@
 ﻿class Program
 {
-    static void RepeatCharacters(string input)
+    static void repeatCharacters(string input)
     {
+        string str = input;
+        var duplicates = new List<char>();
 
+        foreach (char item in str)
+        {
+            int count = 0;
+            foreach (var chars in str)
+            {
+                if (item == chars)
+                {
+                    count++;
+                }
+            }
+
+            if (count > 1 && !duplicates.Contains(item))
+            {
+                duplicates.Add(item);
+            }
+        }
+        Console.WriteLine("[{0}]", string.Join(", ", duplicates).ToLower());
     }
 
     static void Main(string[] args)
     {
         string repeatCharactersString = "Programmatic Python";
-
-        Console.WriteLine(RepeatCharacters(repeatCharactersString);
+        repeatCharacters(repeatCharactersString);
     }
 }
