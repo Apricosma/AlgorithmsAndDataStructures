12b7f5aae65def9bb8464128071843f7770cadf2
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-12 14:50:16 -0600

    Add project files.

---
+++ CapitalizeFirstLettersOfStrings/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+string str1 = "lorem ipsum et es delorum regum";
+
+TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+// the ToTitleCase() method returns a a new string based on the string
+// passed into it and will capitalize every first letter, and lowercase other letters.
+Console.WriteLine("\"{0}\" to titlecase: {1}", str1, textInfo.ToTitleCase(str1));
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿string fizz = "Fizz";
+string buzz = "Buzz";
+int sum = 0;
+double total = 0;
+
+Console.WriteLine("Please input a number");
+double iterator = Convert.ToDouble(Console.ReadLine());
+
+for (int i = 0; i <= iterator; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0 && i != 0)
+    {
+        Console.WriteLine(fizz + buzz);
+    }
+    else if (i % 3 == 0 && i != 0)
+    {
+        Console.WriteLine(fizz);
+    }
+    else if (i % 5 == 0 && i != 0)
+    {
+        Console.WriteLine(buzz);
+    }
+    else
+    {
+        Console.WriteLine(i);
+        sum += i;
+        total++;
+    }
+}
+
+double percentage = total / iterator * 100;
+Console.WriteLine("The sum of every non-fizz or buzz line is: " + sum);
+Console.WriteLine("The total numbers that do not include fizzbuzz is: " + total);
+Console.WriteLine("The percentage of non-fizz and non-buzz is: " + percentage + "%");
