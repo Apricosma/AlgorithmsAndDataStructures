967fa4f8326269736693b705f8e440df99bb2cb9
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-15 12:42:22 -0600

    Add writeline for better formatting on getDuplicateIntegers()

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -3,17 +3,17 @@
     // Find the elements that appear twice in O(n) time
     static void getDuplicateIntegers(int[] array)
     {
-        // create a temporary array for comparison
+        // create a temporary array for storing count values
         int maxVal = array.Max();
         int[] temp = new int[maxVal + 1];
 
         // iterate the array once
         for (int i = 0; i < array.Length; i++)
         {
-            // if element is flagged, print
+            // if element is flagged 1 at the same index, print
             if (temp[array[i]] == 1)
             {
-                Console.WriteLine($"{array[i]}, ");
+                Console.WriteLine($"{array[i]}");
             }
             else
             {
@@ -27,7 +27,7 @@
 
     static void Main(string[] args)
     {
-        int[] myArray = { 4, 3, 2, 7, 8, 13, 17, 20, 12, 20, 20, 2, 3, 1 };
+        int[] myArray = { 4, 3, 2, 7, 8, 13, 17, 20, 12, 20, 2, 3, 1 };
         getDuplicateIntegers(myArray);
     }
 }
