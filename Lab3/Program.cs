a59ca2e3ff58ee8f512da6b4354973793822842e
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-15 12:27:22 -0600

    Add basic logic comments for getDuplicateIntegers

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    // Find the elements that appear twice in O(n) time
+    static void getDuplicateIntegers(int[] array)
+    {
+        // create a temporary array for comparison
+        int[] temp = new int[array.Length];
+
+        // iterate the array once
+        for (int i = 0; i < array.Length; i++)
+        {
+            // if element is flagged, print
+
+            // else add flag
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 7, 9, 2, 4 };
+        getDuplicateIntegers(array);
+    }
+}
