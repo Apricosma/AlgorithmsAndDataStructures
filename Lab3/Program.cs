4776bbdc250c7d0b33dacd394910c8455bd726db
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-15 12:31:17 -0600

    Add duplicate finding, breaks when int is > array length

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -10,7 +10,14 @@
         for (int i = 0; i < array.Length; i++)
         {
             // if element is flagged, print
-
+            if (temp[array[i]] == 1)
+            {
+                Console.WriteLine($"{array[i]}, ");
+            }
+            else
+            {
+                temp[array[i]]++;
+            }
             // else add flag
         }
     }
@@ -18,7 +25,7 @@
 
     static void Main(string[] args)
     {
-        int[] array = { 1, 2, 3, 4, 7, 9, 2, 4 };
-        getDuplicateIntegers(array);
+        int[] myArray = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        getDuplicateIntegers(myArray);
     }
 }
