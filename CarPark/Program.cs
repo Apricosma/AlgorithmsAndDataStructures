2bce8e4ddc6b9e54736809b8ad7123e8f2bb819a
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-17 14:01:19 -0600

    Take input for car park size

---
+++ CarPark/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static List<string> createCarPark(int size)
+    {
+
+        int n = size;
+        List<string> carPark = new List<string>();
+        int i = 0;
+        while (i < n)
+        {
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    if (i < n)
+                    {
+                        carPark.Add(c + j.ToString());
+                        i++;
+                    }
+                }
+            }
+        }
+
+        return carPark;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input the size of the car park");
+        int parkSizeInput = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(String.Join(", ", createCarPark(parkSizeInput)));
+    }
+}
--- HashSets/Program.cs
+++ HashSets/Program.cs
@@ -1,34 +1,34 @@
 ﻿// Reviewing HashSets
-List<string> strings = new List<string>();
+//List<string> strings = new List<string>();
 
-strings.Add("Hello");
-strings.Add("goodbye");
+//strings.Add("Hello");
+//strings.Add("goodbye");
 
-bool hasGoodbye = strings.Contains("goodbye");
+//bool hasGoodbye = strings.Contains("goodbye");
 
 // create a new list for copies
-List<string> allCaps = new List<string>();
+//List<string> allCaps = new List<string>();
 
-foreach(string s in strings)
-{
-    allCaps.Add(s.ToUpper());
-}
+//foreach(string s in strings)
+//{
+//    allCaps.Add(s.ToUpper());
+//}
 
 // if we know we will only have unique values
 // hashsets are better because they have reduced complexity due to black magic (?)
-HashSet<int> ints = new HashSet<int>();
-ints.Add(1);
-ints.Add(2);
+//HashSet<int> ints = new HashSet<int>();
+//ints.Add(1);
+//ints.Add(2);
 
-bool hasTwo = ints.Contains(2);
-Console.WriteLine(ints);
+//bool hasTwo = ints.Contains(2);
+//Console.WriteLine(ints);
 
 
 // dictionaries
 // collection of key : value pairs
 // finding keys is faster 
 
-string testString = "The sixth sheik's sixth sheep is sick";
+//string testString = "The sixth sheik's sixth sheep is sick";
 
 // count the number of each character that appears in this sentence 
 // a: 1, b: 3, c: 16
@@ -60,25 +60,25 @@ string testString = "The sixth sheik's sixth sheep is sick";
 //}
 
 // count coin flips
-List<bool> heads = new List<bool>
-{
-    true, false, true, true, true, false, false
-};
+//List<bool> heads = new List<bool>
+//{
+//    true, false, true, true, true, false, false
+//};
 
-Dictionary<string, int> flipCount = new Dictionary<string, int>();
-flipCount.Add("heads", 0);
-flipCount.Add("tails", 0);
+//Dictionary<string, int> flipCount = new Dictionary<string, int>();
+//flipCount.Add("heads", 0);
+//flipCount.Add("tails", 0);
 
-foreach(bool flip in heads)
-{
-    if(flip)
-    {
-        flipCount["heads"]++;
-    } else
-    {
-        flipCount["tails"]++;
-    }
-}
+//foreach(bool flip in heads)
+//{
+//    if(flip)
+//    {
+//        flipCount["heads"]++;
+//    } else
+//    {
+//        flipCount["tails"]++;
+//    }
+//}
 
 //charCount['a'] = 12;
 //charCount['c'] = 13;
@@ -92,28 +92,63 @@ foreach(bool flip in heads)
 // write a dictionary that counts the number of words that start with different letters in a string
 // "Mary had a little lamb"
 
-string str = "Mary had a little lamb";
-Dictionary<char, int> letterCount = new Dictionary<char, int>();
-var firstLetters = str.Split(" ").Select(s => s[0]);
-foreach (char c in firstLetters)
+//string str = "Mary had a little lamb";
+//Dictionary<char, int> letterCount = new Dictionary<char, int>();
+//var firstLetters = str.Split(" ").Select(s => s[0]);
+//foreach (char c in firstLetters)
+//{
+//    char letter = char.ToLower(c);
+
+//    if (char.IsLetter(letter))
+//    {
+
+//        if (letterCount.ContainsKey(letter))
+//        {
+//            letterCount[letter]++;
+//        }
+//        else
+//        {
+//            letterCount[letter] = 1;
+//        }
+//    }
+//}
+
+
+//foreach (KeyValuePair<char, int> pair in letterCount)
+//{
+//    Console.WriteLine($"The character {pair.Key} appears {pair.Value} times");
+//}
+
+// better 
+
+using System.Text;
+
+string test = "Hello idk what to put here";
+
+string[] words = test.Split(' ');
+
+Dictionary<char, List<string>> wordCollections = new Dictionary<char, List<string>>();
+
+foreach(string word in words)
 {
-    char letter = char.ToLower(c);
+    char firstChar = word.ToLower().First();
 
-    if (char.IsLetter(letter))
+    if (!wordCollections.ContainsKey(firstChar))
     {
-
-        if (letterCount.ContainsKey(letter))
-        {
-            letterCount[letter]++;
-        }
-        else
-        {
-            letterCount[letter] = 1;
-        }
+        wordCollections.Add(firstChar, new List<string>());
     }
+    wordCollections[firstChar].Add(word);
 }
 
-foreach (KeyValuePair<char, int> pair in letterCount)
+foreach (KeyValuePair<char, List<String>> pair in wordCollections) 
 {
-    Console.WriteLine($"The character {pair.Key} appears {pair.Value} times");
+    
+    List<string> wordList = pair.Value;
+    StringBuilder sb = new StringBuilder( wordList.Count );
+    foreach (string word in wordList)
+    {
+        sb.Append( word + " " );
+    }
+
+    Console.WriteLine($"{pair.Key} : {sb}");
 }
