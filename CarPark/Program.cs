201f2ce98bc767dbcd89abe9149678fc5ed5c839
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-17 14:48:52 -0600

    Add check for empty slots

--- CarPark/Program.cs
+++ CarPark/Program.cs
@@ -27,10 +27,43 @@ class Program
         return carPark;
     }
 
+    static Dictionary<string, int> getCarParkOccupancy(List<string> list) {
+
+        Dictionary<string, int> carParkDictionary = new Dictionary<string, int>();
+
+        foreach(string element in list)
+        {
+            carParkDictionary.Add(element, 0);
+        }
+
+        return carParkDictionary;
+    }
+
+    static bool checkIsEmpty(Dictionary<string, int> carPark, string key)
+    {
+        int value;
+        if(carPark.TryGetValue(key, out value))
+        {
+            return value == 0;
+        } else
+        {
+            return false;
+        }
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Input the size of the car park");
         int parkSizeInput = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(String.Join(", ", createCarPark(parkSizeInput)));
+        List<string> carParkList = createCarPark(parkSizeInput);
+
+        Dictionary<string, int> carPark = getCarParkOccupancy(carParkList);
+
+        
+        Console.WriteLine(String.Join(", ", carPark));
+        Console.WriteLine("Select the lot you want");
+
+        Console.WriteLine(checkIsEmpty(carPark, Console.ReadLine()));
     }
 }
