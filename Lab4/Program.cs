eacf42189513df21e2360faaf7deb4de00897dfa
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-17 06:39:12 -0600

    Add GetHighestValueInList() method

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static int GetHighestValueInList(List<int> inputList)
+    {
+        // O(n) 
+        return inputList.Max();
+    }
+
+    //static List<int> MaxNumbersInLists(List<List<int>> inputList)
+    //{
+        
+    //}
+
+    static void Main(string[] args)
+    {
+        List<int> testList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.WriteLine(GetHighestValueInList(testList));
+    }
+}
