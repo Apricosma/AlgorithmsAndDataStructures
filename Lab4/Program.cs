38f59f70c04458746c98c802f504f3eb4f5868fd
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-01-17 07:33:19 -0600

    Add highest grade base output

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -21,20 +21,37 @@ class Program
         return maxIntList;
     }
 
+    static String HighestGrade(List<List<int>> inputList)
+    {
+        int maxGrade = MaxNumbersInLists(inputList).Max();
+        string outputString = $"The highest grade is: {maxGrade.ToString()}";
+        return outputString;
+    }
+
     static void Main(string[] args)
     {
-        List<List<int>> testList = new List<List<int>>();
-        testList.Add(new List<int> { 1, 5, 3 });
-        testList.Add(new List<int> { 9, 7, 3, -2 });
-        testList.Add(new List<int> { 2, 1, 2 });
+        List<List<int>> testList = new List<List<int>>
+        {
+            new List<int> { 1, 5, 3 },
+            new List<int> { 9, 7, 3, -2 },
+            new List<int> { 2, 1, 2 }
+        };
 
-        
+
         List<int> maxList = MaxNumbersInLists(testList);
         for (int i = 0; i < maxList.Count; i++)
         {
             Console.WriteLine($"List {i+1} has a maximum number of: {maxList[i]}");
         }
-        
+
+        List<List<int>> grades = new List<List<int>>
+        {
+            new List<int> { 85, 92, 67, 94, 94 },
+            new List<int> { 50, 60, 57, 95 },
+            new List<int> { 95 }
+        };
+
+        Console.WriteLine(HighestGrade(grades));
     }
 
 }
